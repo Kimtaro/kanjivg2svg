0e3ca33d0daf917736c3b45faa39fa42584e5b2f
Kim Ahlström <kim.ahlstrom@gmail.com>
2010-11-30 15:17:20 +0900

    Trying out doing several frames on the same canvas. Not really working

--- convert.rb
+++ convert.rb
@@ -7,17 +7,19 @@ require 'pp'
 class Importer
   class KanjiVG
     
-    SVG_HEAD = '<svg width="120" height="120" viewBox="-5 -5 120 120" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" version="1.1"  baseProfile="full">'
+    WIDTH = 120
+    HEIGHT = 120
+    SVG_HEAD = "<svg width=\"__WIDTH__\" height=\"#{HEIGHT}\" viewBox=\"-5 -5 __WIDTH__ #{HEIGHT}\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\" version=\"1.1\"  baseProfile=\"full\">"
     SVG_FOOT = '</svg>'
-    TEXT_STYLE = 'fill:#FF2A00;font-family:Helvetica;font-weight:bold;font-size:15;stroke-width:0'
+    TEXT_STYLE = 'fill:#FF2A00;font-family:Helvetica;font-weight:normal;font-size:14;stroke-width:0'
     PATH_STYLE = 'fill:none;stroke:black;stroke-width:3'
-    PATH_START_RE = %r{^M (\d+ (?:\.\d+)?) , (\d+ (?:\.\d+)?)}ix
+    ENTRY_NAME = 'kanji'
+    COORD_RE = %r{(?ix:\d+ (?:\.\d+)?)}
     
-    def initialize(doc, output_dir, animate = false, verbose = false)
-      @verbose = verbose
-      @entry_name = 'kanji'
+    def initialize(doc, output_dir, type = :numbers, verbose = false)
       @output_dir = output_dir
-      @animate = animate
+      @type = type
+      @verbose = verbose
       processed = 0
 
       puts "Starting the conversion @ #{Time.now} ..." if @verbose
@@ -27,9 +29,9 @@ class Importer
       tmp = ""
       begin
         while (line = doc.readline)
-          if line =~ %r{<#{@entry_name}}
+          if line =~ %r{<#{ENTRY_NAME}}
             tmp = line
-          elsif line =~ %r{</#{@entry_name}>}
+          elsif line =~ %r{</#{ENTRY_NAME}>}
             tmp << line
             noko = Nokogiri::XML(tmp)
             parse(noko)
@@ -47,31 +49,71 @@ class Importer
       end
     end
     
+    private
+    
     def parse(doc)
-      doc.css(@entry_name).each do |entry|
+      doc.css(ENTRY_NAME).each do |entry|
         codepoint = entry['id']
-        svg = File.open("#{@output_dir}/U+#{codepoint}#{@animate ? '_animated' : ''}.svg", File::RDWR|File::TRUNC|File::CREAT)
-        svg << "#{SVG_HEAD}\n"
+        svg = File.open("#{@output_dir}/U+#{codepoint}_#{@type}.svg", File::RDWR|File::TRUNC|File::CREAT)
+        
+        if @type == :frames
+          width = WIDTH * entry.css('stroke').length
+        else
+          width = WIDTH * 1
+        end
+        header = SVG_HEAD.gsub('__WIDTH__', width.to_s)
+        svg << "#{header}\n"
+        
         stroke_count = 0
-
+        paths = []
         entry.css('stroke').each do |stroke|
-          md = PATH_START_RE.match(stroke['path'])
-          x = md[1].to_f
-          y = md[2].to_f
-          
-          x -= 5
-          y -= 2
-          
+          paths << stroke['path']
           stroke_count += 1
           
           base_path = "<path d=\"#{stroke['path']}\""
-          if @animate
+          case @type
+          when :animated
             svg << "#{base_path} style=\"#{PATH_STYLE};opacity:0\">\n"
             svg << "  <animate attributeType=\"CSS\" attributeName=\"opacity\" from=\"0\" to=\"1\" begin=\"#{stroke_count-1}s\" dur=\"1s\" repeatCount=\"0\" fill=\"freeze\" />\n"
             svg << "</path>\n"
-          else
+          when :numbers
+            x, y = move_text_relative_to_path(stroke['path'])
             svg << "<text x=\"#{x}\" y=\"#{y}\" style=\"#{TEXT_STYLE}\">#{stroke_count}</text>\n"
             svg << "#{base_path} style=\"#{PATH_STYLE}\" />\n"
+          when :frames
+            md = %r{^[LMT] (#{COORD_RE}) , (#{COORD_RE})}ix.match(paths.last)
+            path_start_x = md[1].to_f
+            path_start_y = md[2].to_f
+            
+            paths.each do |path|
+              path.gsub!(%r{([LMT]) (#{COORD_RE})}x) do |m|
+                letter = $1
+                x  = $2.to_f
+                x += WIDTH
+                "#{letter}#{x}"
+              end
+              path.gsub!(%r{(S) (#{COORD_RE}) , (#{COORD_RE}) , (#{COORD_RE})}x) do |m|
+                letter = $1
+                x1  = $2.to_f
+                x1 += WIDTH
+                x2  = $4.to_f
+                x2 += WIDTH
+                "#{letter}#{x1}#{$3}#{x2}"
+              end
+              path.gsub!(%r{(C) (#{COORD_RE}) , (#{COORD_RE}) , (#{COORD_RE}) , (#{COORD_RE}) , (#{COORD_RE})}x) do |m|
+                letter  = $1
+                x1  = $2.to_f
+                x1 += WIDTH
+                x2  = $4.to_f
+                x2 += WIDTH
+                x3  = $6.to_f
+                x3 += WIDTH
+                "#{letter}#{x1}#{$3}#{x2}#{$5}#{x3}"
+              end
+              
+              svg << "<path d=\"#{path}\" style=\"#{PATH_STYLE}\" />\n"
+            end
+            svg << "\n"
           end
         end
 
@@ -80,8 +122,20 @@ class Importer
       end
     end
     
+    # TODO: make this shit really smart
+    def move_text_relative_to_path(path)
+      md = %r{^M (#{COORD_RE}) , (#{COORD_RE})}ix.match(path)
+      path_start_x = md[1].to_f
+      path_start_y = md[2].to_f
+      
+      text_x = path_start_x
+      text_y = path_start_y
+      
+      [text_x, text_y]
+    end
+    
   end
 end
 
 file = ARGV[0]
-Importer::KanjiVG.new(File.open(file), File.expand_path('../svgs',  __FILE__), true, true)
+Importer::KanjiVG.new(File.open(file), File.expand_path('../svgs',  __FILE__), :frames, true)
